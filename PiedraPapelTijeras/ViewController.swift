e41eb4813c11bfd7ec603ac0965e742d626958f0
rodrigoyaneztec <94147437+rodrigoyaneztec@users.noreply.github.com>
2022-08-23 14:00:45 -0600

    first commit

---
+++ PiedraPapelTijeras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiedraPapelTijeras
+//
+//  Created by Rodrigo Alejandro Yáñez García on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
